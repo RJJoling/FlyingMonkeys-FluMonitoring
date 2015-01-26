d8eb4668f5bfdeb12bcde783f6a7784bdfbc21d5
RJJoling <rjjoling@gmail.com>
2015-01-26 18:31:41 +0100

    Tracer Bullet

---
+++ main.R
@@ -0,0 +1,15 @@
+## Write database to txt or csv
+
+
+
+## Clip dataset to the Netherlands
+
+## Index dataset, filter to data required
+
+## Bucket tweets from municipalites into clusters
+
+## Create monthly averages
+
+## Plot monthly averages
+
+## Create GUI interface to show monthly averages compare to each other
