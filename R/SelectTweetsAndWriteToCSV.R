aa3351a4e9efd89302f905c269f62d7ac75140d3
RJJoling <rjjoling@gmail.com>
2015-01-27 18:16:07 +0100

    Get data from database

---
+++ R/SelectTweetsAndWriteToCSV.R
@@ -0,0 +1,42 @@
+# Import libraries
+library('sp')
+library('rgdal')
+library('rgeos')
+
+
+
+SelectTweetsAndWriteToCSV <- function(list){
+  # Select tweets by keyword
+  for (keyword in list){
+    query = paste("select tweet_text, tweet_datetime, latitude, longitude from tweets2601 where tweet_text like '%", keyword, "%'", sep = "")
+    rs <- dbSendQuery(con, query)
+    df <- fetch(rs,n=-1)
+
+    # Turn dataframe into a spatial points dataframe
+    xy <- cbind(df$longitude, df$latitude)
+    sp.df <- SpatialPointsDataFrame(xy, df, proj4string = CRS("+proj=longlat +ellps=WGS84 +datum=WGS84 +no_defs +towgs84=0,0,0"))
+
+    # Get boundary of the Netherlands
+    datdir <- 'data'
+    dir.create(datdir, showWarnings = FALSE)
+    adm <- raster::getData("GADM", country = "NLD", level = 0, path = datdir)
+
+    # Clip points to boundary of the Netherlands
+    pts.intersect <- gIntersection(adm, sp.df, byid = TRUE)
+
+    # Get row ID of points that are within the intersect
+    pts.intersect.strsplit <- strsplit(dimnames(pts.intersect@coords)[[1]], " ")
+    pts.intersect.id <- as.numeric(sapply(pts.intersect.strsplit, "[[", 2))
+
+    # Match attributes of original dataframe with intersected points, by row ID
+    nld.df <- sp.df[pts.intersect.id, ]
+
+    # Write filtered tweets to .CSV file
+
+      write.csv(nld.df@data, file = paste("data/", keyword, "Tweets.csv", sep = ""), row.names = FALSE)
+  }
+}
+
+# Get the GADM boundaries before the for loop
+# Try to include an OR statment within SQL query
+# Include print statements so that we know where we are at within the getting data process
--- main.R
+++ main.R
@@ -1,10 +1,23 @@
-## Write database to txt or csv
+# Check if required package is installed
+require("RPostgreSQL")
+
+# Import libraries
+library('RPostgreSQL')
+
+# Load functions
+source("R/SelectTweetsAndWriteToCSV.R")
 
 
-
-## Clip dataset to the Netherlands
+# Get driver for database and make connection to it
+drv <- dbDriver("PostgreSQL")
+con <- dbConnect(drv, host='xx', dbname='xx', user='xx', password='xx')
 
 ## Index dataset, filter to data required
+## Clip dataset to the Netherlands
+## Write database to txt or csv
+tweets <- c('griep', 'verkoud', 'koorts')
+
+SelectTweetsAndWriteToCSV(tweets)
 
 ## Bucket tweets from municipalites into clusters
 
@@ -13,3 +26,23 @@
 ## Plot monthly averages
 
 ## Create GUI interface to show monthly averages compare to each other
+
+
+# Disconnect connection and unload driver
+dbDisconnect(con)
+dbUnloadDriver(drv)
+
+# name <- ""
+# for (char in keyword){
+#
+#   if (char in [a-zA-Z]){
+#     name <- paste(name, "")
+#   }
+# }
+#
+#   name <- paste(name, sub("[^[:alpha:]]","",char, sep = "")
+# }
+# if (is.character(char)){
+#   name += char
+# }}
+# paste("data/", name, "Tweets.csv", sep = "")
