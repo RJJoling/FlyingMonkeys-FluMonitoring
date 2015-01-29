ff4962bf47f2f7f02bf78dda0b2003412009be68
RJJoling <rjjoling@gmail.com>
2015-01-29 12:39:46 +0100

    Deletes

--- R/createPNG.R
+++ R/createPNG.R
@@ -1,7 +1,7 @@
 
 
 createPNG <- function(month){
-    png(paste("data/", month, ".png", sep = ""), height = 3000, width = 5000, res = 300)
+    png(paste("output/", month, ".png", sep = ""), height = 3000, width = 5000, res = 300)
     createMap(month)
     dev.off()
 }
